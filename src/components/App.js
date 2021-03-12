79a4b6700d18d3605831255ae32351e3b4c6d873
Randel M <Randel.C.Moore@gmail.com>
2021-03-12 15:09:37 -0800

    add code to send and retrieve data from firestore

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PlantControl from "./PlantControl";
+
+function App() {
+  return (
+    <React.Fragment>
+      <PlantControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
