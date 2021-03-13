881ce3e42fa8a93290aed1eae932487f92191633
Randel M <Randel.C.Moore@gmail.com>
2021-03-12 16:43:59 -0800

    add hardcoded graph data

--- src/components/PlantControl.js
+++ src/components/PlantControl.js
@@ -1,75 +1,24 @@
 import React from "react";
-import firebase from "./../firebase/index";
+import PlantGraph from "./PlantGraph";
+//import firebase from "./../firebase/index";
+
 
 class PlantControl extends React.Component {
   constructor() {
     super();
     this.state = {
-      masterList: null
+      selectedPlant: null
     }
   }
 
-  //ONE TIME GET REQUEST!!! (OUTSIDE of Render() function.)
-  // componentDidMount() {
-  //   firebase.db.collection("hardware").get()
-  //     .then(querySnapshot => {
-  //       const result = querySnapshot.docs.map( doc => {
-  //         return {...doc.data(), id: doc.id};
-  //       })
-  //       this.setState({masterList: result});
-  //     })
-  //     .catch(err => {
-  //       console.log(err.message);
-  //     })
-  // };
-  ////////////////////////////
-
-  //REALTIME LISTENER!!! (OUTSIDE of Render() function)
-  componentDidMount() {
-    const doc = firebase.db.collection("hardware");
-    doc.onSnapshot(docSnapshot => {
-      // console.log(`received doc snapshot: ${docSnapshot}`);
-      const result = docSnapshot.docs.map( doc => {
-      return {...doc.data(), id: doc.id};
-      })
-      this.setState({masterList: result});
-    }, err => {
-      console.log(`Encountered error: ${err}`);
-    });
-  }
-  ////////////////////////////////////////////////
-
-
   render() {
     
-    // ADD DATA  (infinite loop inside render)
-    // firebase.db.collection("testCollection").add({title: "first todo", description: "new todo" })
-    // .then(documentReference => {
-    //   console.log("document reference ID", documentReference.id)
-    // })
-    // .catch(error => {
-    //   console.log(error.message)
-    // });
-
-    // GET DATA (infinite loop inside render)
-    // firebase.db.collection("testCollection").get()
-    // .then(querySnapshot => {
-    //   querySnapshot.docs.map( doc => {
-    //     console.log(doc.id, " => ", doc.data());
-    //   })
-    // })
-    // .catch(err => {
-    //   console.log(err.message)
-    // })
-    
     return (
-    
-      console.log(this.state.masterList),
-      <h1>Placeholder</h1>
-    
+      <React.Fragment>
+        <PlantGraph />
+      </React.Fragment>
     );
   }
-
 }
 
 export default PlantControl
--- src/components/PlantGraph.js
+++ src/components/PlantGraph.js
@@ -1,13 +1,18 @@
 import React from "react";
 import firebase from "./../firebase/index";
+import CanvasJSReact from "./../canvasjs.react"; 
+
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 class PlantGraph extends React.Component {
   constructor() {
     super();
     this.state = {
-      masterPlantList: null
+      masterPlantList: [{moisture: 0, dateTime: "test"}]
     }
   }
+
 
   componentDidMount() {
     const doc = firebase.db.collection("hardware");
@@ -16,13 +21,20 @@ class PlantGraph extends React.Component {
       const result = docSnapshot.docs.map( doc => {
       return {...doc.data(), id: doc.id};
       })
-      this.setState({masterList: result});
+      this.setState({masterPlantList: result});
     }, err => {
       console.log(`There has been an error: ${err}`);
     });
   }
 
-  Render() {
+  myData() {
+    // const dataObject = this.state.masterPlantList;
+    return this.state.masterPlantList.map( dataPoint => {
+      return { x: dataPoint.dateTime, y: dataPoint.moisture }
+    })
+  }
+
+  render() {
     const options = {
       animationEnabled: true,
       title:{
@@ -38,22 +50,83 @@ class PlantGraph extends React.Component {
         type: "spline",
         name: "Your Plant",
         showInLegend: true,
-        dataPoints: this.state.masterPlantList
+        dataPoints: this.myData
+        // [
+        // {x: "0", y: 5},
+        // {x: "1", y: 3},
+        // {x: "2", y: 7},
+        // {x: "3", y: 9},
+        // {x: "4", y: 2},
+        // {x: "5", y: 4},
+        // {x: "6", y: 7},
+        // ]
       }]
     }
+    // const options = {
+		// 	animationEnabled: true,
+		// 	title:{
+		// 		text: "Monthly Sales - 2017"
+		// 	},
+		// 	axisX: {
+		// 		valueFormatString: "MMM"
+		// 	},
+		// 	axisY: {
+		// 		title: "Sales (in USD)",
+		// 		prefix: "$"
+		// 	},
+		// 	data: [{
+		// 		yValueFormatString: "$#,###",
+		// 		xValueFormatString: "MMMM",
+		// 		type: "spline",
+		// 		dataPoints: [
+		// 			{ x: new Date(2017, 0), y: 25060 },
+		// 			{ x: new Date(2017, 1), y: 27980 },
+		// 			{ x: new Date(2017, 2), y: 42800 },
+		// 			{ x: new Date(2017, 3), y: 32400 },
+		// 			{ x: new Date(2017, 4), y: 35260 },
+		// 			{ x: new Date(2017, 5), y: 33900 },
+		// 			{ x: new Date(2017, 6), y: 40000 },
+		// 			{ x: new Date(2017, 7), y: 52500 },
+		// 			{ x: new Date(2017, 8), y: 32300 },
+		// 			{ x: new Date(2017, 9), y: 42000 },
+		// 			{ x: new Date(2017, 10), y: 37160 },
+		// 			{ x: new Date(2017, 11), y: 38400 }
+		// 		]
+		// 	}]
+		// }
+    // const options = {
+    //   title: {
+    //     text: "Temperature in Freedom Units"
+    //   },
+    //   axisY: {
+    //     title: "Temp Fahrenheit",
+    //     minimum: -50,
+    //     maximum: 145,
+    //   },
+    //   data: [{
+    //     type: "column",
+    //     dataPoints: [
+    //       { label: "1", y: 50 },
+    //       { label: "1", y: 50 },
+    //       { label: "1", y: 50 },
+    //       { label: "1", y: 50 },
+    //       { label: "1", y: 50 },
+    //       { label: "1", y: 50 },
+    //       { label: "1", y: 50 },
+    //       { label: "1", y: 50 }
+    //     ]
+    //   }]
+    // }
     
-    if (!isLoaded || !this.state.masterPlantList) {
+    if (!options || !this.state.masterPlantList) {
       return <>Loading...</>
     } else { 
       
       return (
-        <>
-        <hr/>
-        <p class="compBound">PlantGraph Start</p>
-          <CanvasJSChart optons={options} />
-        <p class="compBound">PlantGraph End</p>
-        <hr/>
-        </>
+        <React.Fragment>
+          {console.log(this.state.masterPlantList)}
+          <CanvasJSChart options={options} />
+        </React.Fragment>
       );
     }
   }
